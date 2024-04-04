de0e2c11bb5e1679a67ed755ceb5968cf0c74f40
jmozzart <cyrilnicko@gmail.com>
2024-04-04 12:38:50 +0800

    ok

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,6 +2,7 @@
 import { SetStateAction, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
+import Modal from "./Modal";
 
 
 
@@ -62,6 +63,13 @@ const Carousel = ({ images }: CarouselProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState('left');
   const [isNavClicked, setIsNavClicked] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+      setIsOpen(!isOpen);
+  };
+
+
   const handleNext = () => {
     setDirection("right");
     setCurrentIndex((prevIndex) =>
@@ -98,13 +106,15 @@ const Carousel = ({ images }: CarouselProps) => {
             fill
             quality={50}
             alt="carousel image"
-            loading={currentIndex === 0 ? "eager" : "lazy"}
+            loading={currentIndex < 2 ? "eager" : "lazy"}
+            onClick={toggleModal}
+            className="cursor-pointer"
           />
         </motion.div>
 
       </AnimatePresence>
       {images && images.length > 1 && <div className="flex justify-between">
-        <div className="text-gray-200 absolute top-0 bottom-0 h-6 w-6 rounded-full px-1.5 py-1 mx-auto my-32 lg:my-48 bg-mainaccent-700" onClick={handlePrevious}>
+        <div className="text-gray-200 absolute top-0 bottom-0 h-6 w-6 rounded-full px-1.5 py-1 mx-auto my-32 lg:my-48 bg-mainaccent-700 cursor-pointer" onClick={handlePrevious}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="20"
@@ -115,7 +125,7 @@ const Carousel = ({ images }: CarouselProps) => {
             <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
           </svg>
         </div>
-        <div className="right-0 text-gray-200 absolute top-0 bottom-0 h-6 w-6 rounded-full px-1.5 py-1 mx-auto my-32 lg:my-48 bg-mainaccent-700" onClick={handleNext}>
+        <div className="right-0 text-gray-200 absolute top-0 bottom-0 h-6 w-6 rounded-full px-1.5 py-1 mx-auto my-32 lg:my-48 bg-mainaccent-700 cursor-pointer" onClick={handleNext}>
           <svg height="20" viewBox="0 96 960 960" width="20"
             className="fill-white"
           >
@@ -132,6 +142,14 @@ const Carousel = ({ images }: CarouselProps) => {
           ></div>
         ))}
       </div> */}
+      {isOpen && <Modal isOpen={isOpen} onClose={toggleModal}>
+        <Image src={images[currentIndex]} alt="carousel image"
+        width={1000}
+        height={1000}
+         />
+      </Modal>
+
+      }
     </div>
   );
 
--- src/utils/sanityClient.ts
+++ src/utils/sanityClient.ts
@@ -4,7 +4,7 @@ import {createClient} from 'next-sanity'
 export default createClient({
     projectId: 'xle5b8xj', 
     dataset: 'checkdataset', 
-    useCdn: false,
+    useCdn: true,
     //apiVersion: '2022-03-07',
     apiVersion: "2024-01-01",
 })
